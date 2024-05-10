b1c641dcc59c239c32d00fa7311878a15c445d6e
Danilo17025 <116519718+Danilo17025@users.noreply.github.com>
2024-05-10 18:59:55 +0200

    Add files via upload

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css'] // Aquí corregimos el nombre del atributo
+})
+export class FormComponent {
+  // Aquí puedes agregar lógica si es necesaria
+}
